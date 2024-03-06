45659de5797d75794161b95a3e359937f5fe4e96
Henrietta Hosek <92624638+hosekhenrietta@users.noreply.github.com>
2024-03-06 14:17:49 +0100

    Delete feladatok directory

--- feladatok/3. feladat/script.js
+++
@@ -1,6 +0,0 @@
-function delegate(parent, type, selector, handler) {
-    parent.addEventListener(type, function (event) {
-        const targetElement = event.target.closest(selector)
-        if (this.contains(targetElement)) handler.call(targetElement, event)
-    })
-}
