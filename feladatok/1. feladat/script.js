90b94d0520ded5f92e15464db6019fa5740de274
Henrietta Hosek <92624638+hosekhenrietta@users.noreply.github.com>
2024-03-06 14:16:04 +0100

    Add files via upload

---
+++ feladatok/1. feladat/script.js
@@ -0,0 +1,40 @@
+const numbers = [123, 2, -53, 18, 9, 11, 0, 99, 8]
+
+ const matrix = [
+   [10, -2, 8],
+   [51, 4, 9],
+   [-4, 15, 6],
+   [-1, 0, 1],
+ ]
+
+ const movies = [
+    {
+        "Title": "The Avengers",
+        "Year": "2012"
+    },
+    {
+        "Title": "Guardians of the Galaxy",
+        "Year": "2014"
+    },
+    {
+        "Title": "Avengers: Age of Ultron",
+        "Year": "2015"
+    },
+    {
+        "Title": "Black Panther",
+        "Year": "2018"
+    },
+    {
+        "Title": "Avengers: Infinity War",
+        "Year": "2018"
+    },
+    {
+        "Title": "Captain Marvel",
+        "Year": "2019"
+    },
+    {
+        "Title": "Avengers: Endgame",
+        "Year": "2019"
+    }
+]
+
